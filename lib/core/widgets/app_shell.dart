ceb306272a1640ecbe2441b09496659b63e6fc64
Mustafa Beyazbulut <77739828+mustafabeyazbulut@users.noreply.github.com>
2026-09-14 12:04:44 +0300

    Remove single-item bottom nav bar (Flutter requires 2+ destinations)

--- lib/core/widgets/app_shell.dart
+++ lib/core/widgets/app_shell.dart
@@ -9,16 +9,6 @@ class AppShell extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(child: child),
-      bottomNavigationBar: NavigationBar(
-        selectedIndex: 0,
-        destinations: const [
-          NavigationDestination(
-            icon: Icon(Icons.storefront_outlined),
-            selectedIcon: Icon(Icons.storefront_rounded),
-            label: 'Şubeler',
-          ),
-        ],
-      ),
     );
   }
 }
